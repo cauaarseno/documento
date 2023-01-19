7b6536dd3bb9dd6bd0a92dc7f0a65b8ed92f5296
Cauã Arseno <cauaarseno61@gmail.com>
2023-01-19 01:14:21 -0300

    first commit

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    public function document(Request $request)
+    {
+       $name = $request->file->name;
+       $request->file->storeAs('public/documents', $name);
+    }
+}
